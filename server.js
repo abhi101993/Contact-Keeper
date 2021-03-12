9b91823637b2b068f71777198dd17d5c8a2b488c
Abhishek <abhi101993@gmail.com>
2021-03-13 03:40:27 +0530

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db  = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,13 +8,18 @@ db();
 
 app.use(express.json({extended:false}));
 
-app.get('/',(req,res)=>res.json({msg:'Welcome to the ContactKeeper API...'}));
 
 //Define Routesclear
 app.use('/api/users',require('./routes/users'));
 app.use('/api/contacts',require('./routes/contacts'));
 app.use('/api/auth',require('./routes/auth'));
 
+if(process.env.NODE_ENV=== 'production')
+{
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'cliend','build','index.html')));
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,() => console.log(`Server started on port ${PORT}`));
